228ae7f8c823c36a36e65826495628f436c9d67e
Flórián <hennelflori@gmail.com>
2021-02-10 12:06:53 +0100

    modellek

---
+++ elsoapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    kontextus = {
+    "a": 123,
+    "b": "blablabla",
+    "l": [1,3,5,7,9],
+    }
+
+    print(f"a kontextusban az a értéke: {kontextus['a']}")
+    return render(request, "home.html", kontextus) 
