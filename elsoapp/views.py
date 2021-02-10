66d273cb590a0c782053e56e59c83fd0c7c64ada
Flórián <hennelflori@gmail.com>
2021-02-10 12:53:00 +0100

    fájlból eltárolja a tanulókat

--- elsoapp/views.py
+++ elsoapp/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Tanulo, Valasztas, Foglalkozas
+
 def home_view(request, *args, **kwargs):
+
+    tanulok= Tanulo.objects.all()
     kontextus = {
     "a": 123,
     "b": "blablabla",
     "l": [1,3,5,7,9],
+    "tanulo" : tanulok,
     }
 
     print(f"a kontextusban az a értéke: {kontextus['a']}")
